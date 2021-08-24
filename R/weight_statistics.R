da2ed998a556dec16f090e6957dee731f87cf1e6
elena-priego <elena.priego.saiz@gmail.com>
2021-08-24 13:22:27 +0200

    function to generate the statistic in weight loss curve

---
+++ R/weight_statistics.R
@@ -0,0 +1,65 @@
+#' weight_statistics
+#' Function to perform the statistic analysis of the weight-loss curve experiments.
+#'
+#' @param table_tidy input table in tidy format with columns for genotype, day, value and mice.
+#' @param path_to_save path where the output txt will be saved (path_output from the path_builder function).
+#' @param file_name Name of the output file. "statistic.txt" by default.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'weight_statistics(table_raw, path_to_save = path_output)
+#'
+
+weight_statistics <-
+  function(table_tidy,
+           path_to_save = path_output,
+           file_name = "statistic.txt") {
+    output <- here::here(path_to_save, file_name)
+
+    outliers <- table_tidy %>%
+      group_by(genotype, day) %>%
+      identify_outliers(value)
+    print(outliers)
+    cat("Outliers:\n", file = output) #first in the file, without append = TRUE
+    suppressWarnings(
+      write.table(
+        outliers,
+        output,
+        quote = FALSE,
+        sep = "\t",
+        append = TRUE,
+        row.names = FALSE
+      )
+    )
+
+    anova <- table_tidy %>%
+      mutate(day = as.factor(day)) %>%
+      aov(value ~ genotype * day, data = .)
+    anova <- summary(anova)
+    print(anova)
+    cat("\n\n\nAnova:\n", file = output, append = TRUE)
+    capture.output(anova, file = output, append = TRUE)
+
+    one.way <- table_tidy %>%
+      mutate(day = as.factor(day)) %>%
+      group_by(day) %>%
+      anova_test(value ~ genotype) %>%
+      get_anova_table() %>%
+      adjust_pvalue(method = "bonferroni")
+    print(one.way)
+    cat("\n\n\nPost-hoc test(Bonferroni):\n",
+        file = output,
+        append = TRUE)
+    suppressWarnings(
+      write.table(
+        one.way,
+        output,
+        quote = FALSE,
+        sep = "\t\t",
+        append = TRUE,
+        row.names = FALSE
+      )
+    )
+  }
