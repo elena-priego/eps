394eca80ffde3c6f120da65e535d10ec08ce0180
Elena Priego Saiz <epriego@cnic.es>
2022-02-08 16:03:37 +0100

    change path_data to include the output files in data folder and not in main folder starting with data

--- R/path_builder.R
+++ R/path_builder.R
@@ -18,7 +18,7 @@
 path_builder <- function(experiment_name){
   experiment_path <- here::here(experiment_name)
   dir.create(experiment_path, showWarnings = FALSE)
-  path_data <<- here::here(experiment_path, "data")
+  path_data <<- here::here(experiment_path, "data", "")
   dir.create(path_data, showWarnings = FALSE)
   path_raw <<- here::here(experiment_path, "raw")
   dir.create(path_raw, showWarnings = FALSE)
