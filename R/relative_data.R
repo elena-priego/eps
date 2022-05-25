c70be7436aa36f0eb82e2e887832721d9a113bbe
Elena Priego Saiz <epriego@cnic.es>
2022-05-25 19:15:15 +0200

    generalize to different number of columns

--- R/relative_data.R
+++ R/relative_data.R
@@ -1,35 +1,40 @@
 #' Make relative data to genotype
-#' Make relative data to 1 for plotting data in VHL paper. To improve: allow column selection to group_by and pivot_lonter
+#' Make relative data to WT samples from each experiment. representing this as 1.
 #'
-#' @param df input dataframe
+#' @param df input dataframe with at least genotype and value
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' my_data %>% relative_data(.) %>% genotype_violin()
+#'
 relative_data <- function(df) {
-  
+
+  df_groups <- colnames(df) %>% setdiff(., c("genotype", "value"))
+
   table1 <- df %>%
-    group_by(marker, experiment) %>%
+    mutate(strain = factor(str_replace_all(genotype, "-WT|-KO", ""))) %>%
+    group_by_at(df_groups) %>%
     filter(str_detect(genotype, "WT")) %>%
     summarize(mean = sum(value) / n(), .groups = "keep")
-  
+
   table2 <- df %>%
     pivot_wider(names_from = genotype,
                 values_from = value,
                 values_fn = list)
-  
+
   full_table <- left_join(table1, table2)
-  
+
   table3 <- full_table %>%
     pivot_longer(
       names_to = "genotype",
-      cols = -c(marker, experiment, mean),
+      cols = c("genotype", "value"),
       values_drop_na = TRUE
     ) %>%
     unnest(cols = everything()) %>%
     mutate(raw_value = value,
            value = raw_value / mean)
-  
+
   return(table3)
 }
