5b10be42eca6e1523c0715603fbc6c15fd3d386f
Elena Priego Saiz <epriego@cnic.es>
2022-05-27 18:31:35 +0200

    add posibility to select groups to relativize

--- R/relative_data.R
+++ R/relative_data.R
@@ -2,6 +2,8 @@
 #' Make relative data to WT samples from each experiment. representing this as 1.
 #'
 #' @param df input dataframe with at least genotype and value
+#' @param group_diff text to remove from the genotype column to generate the strain by which the relativation groups will be generated. Default to "-WT|-KO"
+#' @param group_control text that identify the control group to relative by. Should be included inside group_diff. Default to "WT"
 #'
 #' @return
 #' @export
@@ -9,15 +11,15 @@
 #' @examples
 #' my_data %>% relative_data(.) %>% genotype_violin()
 #'
-relative_data <- function(df) {
+relative_data <- function(df, group_diff = "-WT|-KO", group_control = "WT") {
 
   df_groups <- colnames(df) %>% setdiff(., c("genotype", "value", "mice"))
 
   table1 <- df %>%
-    mutate(strain = factor(str_replace_all(genotype, "-WT|-KO", "")),
+    mutate(strain = factor(str_replace_all(genotype, group_diff, "")),
            experiment = as.character(experiment)) %>%
     group_by_at(df_groups) %>%
-    filter(str_detect(genotype, "WT")) %>%
+    filter(str_detect(genotype, group_control)) %>%
     summarize(mean = sum(value) / n(), .groups = "keep")
 
   table2 <- df %>%
