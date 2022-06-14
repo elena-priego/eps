2019916e4c5269dfb738f0e3fbcdebf6ed39c2ef
Elena Priego Saiz <epriego@cnic.es>
2022-06-14 20:52:46 +0200

    New options to filter and leyend in markdown

--- R/genotype_boxplot.R
+++ R/genotype_boxplot.R
@@ -1,8 +1,8 @@
-#' Violin plot by genotype
+#' Boxplot plot by genotype
 #'
-#' Violin for tidy standard data by genotype
+#' Boxplot to plot tidy standard data by genotype
 #'
-#' @param table tidy table with time, mice, genotype, treatment, marker, stat, value, experiment and cell columns
+#' @param table tidy table coming form facs_tidytable
 #' @param organ.i optional organ selected to plot (specimen in .fcs file)
 #' @param stat.i optional statistic selected to plot
 #' @param marker.i optional marker selected to plot
@@ -12,39 +12,46 @@
 #' @param genotype_levels vector will all the genotypes all the analysis
 #' @param genotype_labels name to be display in the legend. In markdown/html format.
 #' @param x_value column name to plot
-#' @param x_lab X-axis label
+#' @param title_lab title of the plot
+#' @param x_lab x-axis label
 #' @param y_lab y-axis label
 #' @param y_limit inferior limit for y-axis
-#' @param title_lab title label
-#' @param y_trans transformation of the y axis
-#' @param y_label default to waiver. Could be scientific_format()
-#' @param x_angle Angle to display the x axis
-#' @param x_hjust Justificacion of the x axis
-#' @param leyend_position Legend position. Default to top. Could also be right, left or bottom
-#' @param color_values color to be ploted. Same number as levels have genotype.
-#' @param shape_values shape to be ploted. Same number as levels have genotype.
+#' @param x_angle angle of the labels of the x-axis. NULL for horizontal,
+#' 45 for inclination
+#' @param x_hjust horizontal justification of the labels of the x-axis
+#' @param color_values color to be ploted. Same number as levels have genotype .
+#' @param shape_values shape to be ploted. Same number as levels have genotype. For
 #' @param fill_values fill color to be ploted. Same number as levels have genotype.
 #' @param path_output Optional. Full file name desired (e.g. here(path_output, "plot.pdf"))
 #' @param w width of the output plot
 #' @param h high of the output plot
-#' @param print_plot Boolean indicating if the plot is printed or not. Default to TRUE.
+#' @param print_plot boolean indicating if the plot is printed or not. Default to TRUE.
+
+
 #'
 #' @import here
 #' @import tidyverse
 #' @import ggtext
 #'
-#'
 #' @return plot file in data folder
 #' @export
 #'
 #' @examples
-#' genotype_violin(genotype_levels = VHL_table$genotypes,
-#' color_values = VHL_table$palette_color,
-#' shape_values = VHL_table$palette_shape,
-#' fill_values = VHL_table$palette_fill)
+#' comb <- as_tibble(unique(paste(table$organ, table$stat, table$marker,
+#'                                sep = "_-_"))) %>%
+#'         separate(value, into = c("organ", "stat", "marker"), sep = "_-_") %>%
+#'         mutate(output = here(path_output,
+#'                              paste0(organ, "_", stat, "_", marker, ".png")),
+#'                y_lab = paste0(marker, " (", stat, ")")) %>% as.data.frame()
 #'
-genotype_violin <-
-  function(table,
+#' apply(comb, 1, function(x) facs_boxplot(table, organ.i = x[1], stat.i = x[2],
+#'   marker.i = x[3], path_output = x[4], y_lab = x[5],
+#'   title.i = x[1]))
+
+
+
+facs_boxplot <-
+  function(table ="",
            organ.i = NULL,
            stat.i = NULL,
            time.i = NULL,
@@ -53,16 +60,14 @@ genotype_violin <-
            treatment.i = NULL,
            genotype_levels = c("WT", "KO"),
            genotype_labels = genotype_levels,
-           x_value = "genotype",
+           x_value = "cell",
+           title_lab = "",
            x_lab = "",
            y_lab = "",
-           title_lab = "",
-           y_trans = "identity",
            y_limit = 0,
-           x_angle = 90,
-           x_hjust = 0.5,
-           x_labels = waiver(),
-           leyend_position = "top",
+           y_trans = "identity",
+           x_angle = 45,
+           x_hjust = 1,
            color_values = colorRamps::primary.colors(),
            shape_values = rep(21, 200),
            fill_values = color_values,
@@ -80,28 +85,26 @@ genotype_violin <-
       mutate(genotype = factor(genotype,
                                levels = genotype_levels,
                                labels = genotype_labels)) %>%
-      group_by(genotype, experiment) %>%
-      ggplot(aes(
-        get(x_value),
-        value,
-        fill = genotype,
-        color = genotype,
-        shape = genotype
-      )) +
-      geom_violin(alpha = 0.5,
-                  size = 0.5,
-                  weight = 2) +
+      ggplot(
+        aes(get(x_value),
+            value,
+            fill = genotype,
+            color = genotype,
+            shape = genotype)) +
+      geom_boxplot(outlier.shape = NA,
+                   fill = "transparent",
+                   size = 0.5) +
       geom_point(
         position = position_jitterdodge(jitter.width = 0.3),
         alpha = 0.7,
         size = 3,
         stroke = 0
       ) +
-      # ggrepel::geom_text_repel(aes(label = experiment))+ 3 to plot experiment
       scale_y_continuous(trans = y_trans,
                          expand = expansion(mult = c(0, .1))) +
       expand_limits(y = y_limit) +
-      scale_x_discrete(labels = x_labels) +
+      scale_x_discrete(labels = waiver()) +
+      theme_clean(base_family = "sans", base_size = 11)  +
       labs(
         shape = " ",
         fill = " ",
@@ -110,9 +113,9 @@ genotype_violin <-
         y = y_lab,
         title = title_lab
       ) +
-      theme_clean(base_family = "sans", base_size = 11) +
       theme(
-        legend.position = leyend_position,
+        strip.text.x = element_blank(),
+        legend.position = "right",
         legend.background = element_rect(colour = "transparent",
                                          fill = "transparent"),
         legend.title = element_markdown(face = "plain", size = 9),
@@ -121,8 +124,7 @@ genotype_violin <-
         plot.title = element_markdown(face = "plain", size = 10),
         plot.background = element_rect(colour = NA,
                                        fill = "transparent"),
-        axis.title.y = element_markdown(),
-        axis.title.x = element_markdown()
+        axis.title.y = element_markdown()
       ) +
       scale_shape_manual(values = shape_values,
                          drop = FALSE) +
@@ -138,7 +140,6 @@ genotype_violin <-
         bg = "transparent"
       )
     }
-    if (print_plot == TRUE)
-      plot(p)
+    if (print_plot == TRUE) plot(p)
     return(p)
   }
